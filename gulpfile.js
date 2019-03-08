ce121ca09cc01b747ef5d1d2e624ed729995994c
Fabian Berg <fabian.berg@inovex.de>
2019-03-08 16:57:41 +0100

    reformat gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp');
 var rename = require('gulp-rename');
 var download = require('gulp-download');
 var jsMin = require('gulp-jsmin');
-gulp.task('update:jsbridge', function () {
+
+gulp.task('update:jsbridge', () => {
     return download('https://www.resco.net/downloads/JSBridge.js')
         .pipe(gulp.dest('.'))
         .pipe(jsMin())
